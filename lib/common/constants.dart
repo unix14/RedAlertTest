45de7d1d2ab01d281fc23f952b3e9836827249c4
Eyal Yaakobi <unix14@gmail.com>
2023-12-09 20:59:50 +0200

    Add changes for web and non-web platforms
    - Introduce conditional import for http package based on the platform
    - Modify code for Android by commenting out web-related imports and BrowserClient usage
    - Add TODOs and comments for better understanding and future improvements
    - Known issue: Cookies not received from the server, further exploration needed
    - Investigate and learn from the unofficial Red Alert app's code for potential solutions

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,6 +1,6 @@
 class RedAlertConstants {
-  static const String host = "https://www.oref.org.il";
-  static const String suffix = "/WarningMessages/alert/alerts.json";
+  static const String host = "http://www.oref.org.il";
+  static const String suffix = "/WarningMessages/Alert/alerts.json";
   static const String alertsEndpoint = "$host$suffix";
 // Add other constants as needed
 }
